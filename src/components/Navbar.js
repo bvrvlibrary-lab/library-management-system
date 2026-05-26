f7902c46274a6d8bdf483a9c96ee9aa733988c97
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-05-26 18:16:51 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,143 +4,86 @@ import Link from 'next/link';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 
-export default function Navbar({
-  isAdmin,
-  user
-}) {
+export default function Navbar({ isAdmin, user }) {
   const handleLogout = async () => {
-  await signOut(auth);
+    await signOut(auth);
+    window.location.href = '/login';
+  };
 
-  window.location.href = '/login';
-};
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm px-3">
       <div className="container-fluid">
 
-        <Link
-          href="/"
-          className="navbar-brand fw-bold"
-        >
-          LMS Portal
+        {/* Logo / Brand */}
+        <Link href="/" className="navbar-brand fw-bold">
+          Library System
         </Link>
 
+        {/* Mobile Toggle */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          data-bs-target="#navbarContent"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div
-          className="collapse navbar-collapse"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav ms-auto">
+        {/* Navbar Content */}
+        <div className="collapse navbar-collapse" id="navbarContent">
 
+          {/* Left Side */}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            {/* Home */}
             <li className="nav-item">
-              <Link
-                href="/"
-                className="nav-link"
-              >
+              <Link href="/" className="nav-link">
                 Home
               </Link>
             </li>
 
-            <li className="nav-item">
-              <Link
-                href="/books"
-                className="nav-link"
-              >
-                Library Books
-              </Link>
-            </li>
-
-            {!user && (
+            {/* Student Links */}
+            {!isAdmin && user && (
               <li className="nav-item">
-                <Link
-                  href="/login"
-                  className="nav-link"
-                >
-                  Login
+                <Link href="/mybooks" className="nav-link">
+                  My Books
                 </Link>
               </li>
             )}
 
+            {/* Admin Links */}
             {isAdmin && (
-  
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
+              <li className="nav-item">
+                <Link
+                  href="/admin/dashboard"
+                  className="nav-link"
                 >
-                  Admin
-                </a>
-
-                <ul className="dropdown-menu">
-
-                  <li>
-                    <Link
-                      href="/admin/dashboard"
-                      className="dropdown-item"
-                    >
-                      Dashboard
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link
-                      href="/admin/students"
-                      className="dropdown-item"
-                    >
-                      Student Approval
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link
-                      href="/admin/history"
-                      className="dropdown-item"
-                    >
-                      Books History
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link
-                      href="/admin/issued"
-                      className="dropdown-item"
-                    >
-                      Issued Books
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link
-                      href="/admin/returned"
-                      className="dropdown-item"
-                    >
-                      Returned Books
-                    </Link>
-                  </li>
-
-                </ul>
+                  Admin Dashboard
+                </Link>
               </li>
             )}
-            {user && (
-  <li className="nav-item">
-    <button
-      onClick={handleLogout}
-      className="btn btn-danger btn-sm ms-3"
-    >
-      Logout
-    </button>
-  </li>
-)}
           </ul>
+
+          {/* Right Side */}
+          <div className="d-flex align-items-center gap-3">
+
+            {/* User Email */}
+            {user && (
+              <span className="text-light small">
+                {user.email}
+              </span>
+            )}
+
+            {/* Logout */}
+            {user && (
+              <button
+                onClick={handleLogout}
+                className="btn btn-outline-light btn-sm"
+              >
+                Logout
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </nav>
