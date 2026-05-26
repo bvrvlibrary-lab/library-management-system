aeec52ecafd5c7e1001a1bb053040c16c2fefe7a
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-05-26 17:50:00 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,131 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function Navbar({
+  isAdmin,
+  user
+}) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      <div className="container-fluid">
+
+        <Link
+          href="/"
+          className="navbar-brand fw-bold"
+        >
+          LMS Portal
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ms-auto">
+
+            <li className="nav-item">
+              <Link
+                href="/"
+                className="nav-link"
+              >
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                href="/books"
+                className="nav-link"
+              >
+                Library Books
+              </Link>
+            </li>
+
+            {!user && (
+              <li className="nav-item">
+                <Link
+                  href="/login"
+                  className="nav-link"
+                >
+                  Login
+                </Link>
+              </li>
+            )}
+
+            {isAdmin && (
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                >
+                  Admin
+                </a>
+
+                <ul className="dropdown-menu">
+
+                  <li>
+                    <Link
+                      href="/admin/dashboard"
+                      className="dropdown-item"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/admin/students"
+                      className="dropdown-item"
+                    >
+                      Student Approval
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/admin/history"
+                      className="dropdown-item"
+                    >
+                      Books History
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/admin/issued"
+                      className="dropdown-item"
+                    >
+                      Issued Books
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/admin/returned"
+                      className="dropdown-item"
+                    >
+                      Returned Books
+                    </Link>
+                  </li>
+
+                </ul>
+              </li>
+            )}
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
