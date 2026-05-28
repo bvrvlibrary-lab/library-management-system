85dfce3a5c523c36d35ecd028eb0cd162b91a631
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-05-28 14:03:33 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -419,9 +419,9 @@ const filteredBooks = books.filter((book) => {
   </div>
 </div>
 
-{/*  <div className="row g-3 mb-4">
+//<div className="row g-3 mb-4">
 
-  <div className="col-md-3">*/}
+ // <div className="col-md-3">
    <div className="row g-4 mb-4">
 
   <div className="col-md-3">
@@ -505,7 +505,7 @@ const filteredBooks = books.filter((book) => {
     </div>
   </div>
 
-{/* <div className="col-md-3">*/}
+//<div className="col-md-3">
     <div className="row g-4 mb-4">
 
   <div className="col-md-3">
@@ -589,7 +589,7 @@ const filteredBooks = books.filter((book) => {
     </div>
   </div>
 
-{/*<div className="col-md-3">*/}
+//<div className="col-md-3">
   <div className="row g-4 mb-4">
 
   <div className="col-md-3">
@@ -673,7 +673,7 @@ const filteredBooks = books.filter((book) => {
     </div>
   </div>
 
-{/* <div className="col-md-3">*/}
+// <div className="col-md-3">
    <div className="row g-4 mb-4">
 
   <div className="col-md-3">
