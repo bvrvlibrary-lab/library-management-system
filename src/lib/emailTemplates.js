9389e3b4a43a741070c07e66e90f734f30c5ecf6
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-07-02 09:54:15 +0530

    Create emailTemplates.js

---
+++ src/lib/emailTemplates.js
@@ -0,0 +1,20 @@
+export function issueBookTemplate(
+  request,
+  issueDate,
+  dueDate
+) {
+  return `...`;
+}
+
+export function renewalTemplate(
+  request,
+  newDueDate
+) {
+  return `...`;
+}
+
+export function returnTemplate(
+  request
+) {
+  return `...`;
+}
