96428198cbafe4b36bd08f87865174502cca6c8f
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-07-07 19:19:03 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,25 @@
-import './globals.css'
+import './globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import BootstrapClient from './BootstrapClient';
+
 export const metadata = {
   title: 'Library Management System',
   description: 'PRD Cloud Native LMS App',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-
-        {/* Bootstrap CSS */}
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-        />
-
-      </head>
-
       <body style={{ backgroundColor: '#f8f9fa' }}>
 
+        <BootstrapClient />
 
-  <main className="container-fluid p-0">
-    {children}
-  </main>
+        <main className="container-fluid p-0">
+          {children}
+        </main>
 
-</body>    </html>
+      </body>
+    </html>
   );
 }
