df99c53934b0df3e6208f888299eeb836e26d159
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-07-07 20:27:01 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 
 export const metadata = {
-  title: "BVRV Library",
+  title: "Bhaktivedanta Rajavidyalaya Library",
   description:
     "Bhaktivedanta Rajavidyalaya Library Management System",
 
