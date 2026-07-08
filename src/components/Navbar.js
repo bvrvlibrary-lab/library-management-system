da1c01139eb536819e92e84883a5c42ac012cc05
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-07-08 11:13:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,8 +30,8 @@ export default function Navbar({ isAdmin, user }) {
   <Image
     src="/logo.png"
     alt="BVRV Library"
-    width={60}
-    height={60}
+    width={70}
+    height={70}
   />
 
   <span
