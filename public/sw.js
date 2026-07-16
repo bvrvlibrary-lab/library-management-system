2e7c69ae061befdd0cf18fd1a7eb9ba170299f26
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-07-16 17:41:59 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'bvrv-library-v1';
+
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
