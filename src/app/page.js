a2fa279c73dea981edad6e46306e06bac66a6dfe
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-08-04 07:38:05 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -445,10 +445,7 @@ const filteredBooks = books
       isAdmin={isAdmin}
       user={user}
     />
- <BottomNavbar
-  isAdmin={isAdmin}
-  user={user}
-/>
+
     <div className="container mt-4">
         
      <div
