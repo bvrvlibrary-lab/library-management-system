51c3743805a03b9800fc5dcd470ceb90fdf2d238
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-08-05 14:22:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,9 +14,9 @@ export default function Navbar({ isAdmin, user }) {
  return (
   <>
 <nav
-  className={`navbar navbar-expand-lg shadow-sm ${
-    (user && !isAdmin) ? "student-navbar" : ""
-  }`}
+  cclassName={`navbar navbar-expand-lg shadow-sm ${
+  !isAdmin ? "student-navbar" : ""
+}`}
   style={{
   background:
     'linear-gradient(90deg,#4e342e,#6d4c41)'
