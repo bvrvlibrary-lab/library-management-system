abfe2ea9f99f29e63eda76b1e9b904416088fc1e
bvrvlibrary-lab <bvrvlibrary@gmail.com>
2026-08-05 14:56:02 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,11 @@ export default function RootLayout({ children }) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
-
+            {/* Bootstrap Icons */}
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+/>
         {/* Bootstrap JS */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
